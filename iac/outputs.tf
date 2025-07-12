c6c864b9a690cbac32d91548e66831e209c53376
meadowbees <80916389+meadowbees@users.noreply.github.com>
2025-07-12 13:43:30 -0400

    Update outputs.tf

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,5 +1,6 @@
 output "ecr_url" {
-  value = aws_ecr_repository.app_repo[0].repository_url
+  #value = aws_ecr_repository.app_repo[0].repository_url
+value = local.ecr_repo_url
 }
 
 output "ecs_service_name" {

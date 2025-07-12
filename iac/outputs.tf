6ff9c5081bd8a16600bd02fa0dcab900c7aa2e6e
Hayley Chavez <104518404+hchavez8@users.noreply.github.com>
2025-07-12 13:48:05 -0600

    Update outputs.tf
    Added .app to value = aws_instance.app.public_ip

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -12,7 +12,7 @@ output "alb_dns_name" {
   value       = aws_lb.app_alb.dns_name
 }
 output "instance_public_ip" {
-  value = aws_instance.public_ip
+  value = aws_instance.app.public_ip
 }
 
 #output "app_url" {

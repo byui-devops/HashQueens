bf69f1f47e96a8a80a67fea2a99c18be82cbeceb
Hayley Chavez <104518404+hchavez8@users.noreply.github.com>
2025-07-12 12:29:56 -0600

    Update outputs.tf
    Added some new outputs for instance_public_ip and app_url.

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -11,3 +11,10 @@ output "alb_dns_name" {
   description = "Public DNS name of the Application Load Balancer"
   value       = aws_lb.app_alb.dns_name
 }
+output "instance_public_ip" {
+  value = aws_instance.app_server.public_ip
+}
+
+output "app_url" {
+  value = "http://${aws_instance.app_server.public_ip}:8000"
+}

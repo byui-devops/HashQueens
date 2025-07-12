ba1a5f75616e43185d0ceef6617b5b202e5de4e4
meadowbees <80916389+meadowbees@users.noreply.github.com>
2025-07-11 23:48:32 -0400

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "public_a" {
 resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-2b"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
 

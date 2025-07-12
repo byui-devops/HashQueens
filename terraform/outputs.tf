18b8ed26f531b44d24031263171df703c8c5e25f
meadowbees <80916389+meadowbees@users.noreply.github.com>
2025-07-11 21:42:35 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,3 +8,8 @@ output "ecs_service_name" {
 output "alb_dns_name" {
   value = aws_lb.app_alb.dns_name
 }
+
+output "alb_dns_name" {
+  description = "Public DNS name of the Application Load Balancer"
+  value       = aws_lb.app_alb.dns_name
+}

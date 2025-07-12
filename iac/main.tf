62b41b6641a7a83e23422fa211e35d31cdc94c14
Hayley Chavez <104518404+hchavez8@users.noreply.github.com>
2025-07-12 14:28:45 -0600

    Update main.tf
    Added a security group for load balancer.

--- iac/main.tf
+++ iac/main.tf
@@ -59,6 +59,26 @@ resource "aws_route_table_association" "public_assoc_b" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_security_group" "lb_sg" {
+  name        = "load-balancer-sg"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_security_group" "web_sg" {
   vpc_id = aws_vpc.main.id
 

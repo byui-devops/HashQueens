467d6eacc33e6e10cd2100d63caab1c477ff4649
Hayley Chavez <104518404+hchavez8@users.noreply.github.com>
2025-07-11 19:55:12 -0600

    Update and rename outputs.tf to outputs.tf

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -5,9 +5,6 @@ output "ecr_url" {
 output "ecs_service_name" {
   value = aws_ecs_service.app.name
 }
-output "alb_dns_name" {
-  value = aws_lb.app_alb.dns_name
-}
 
 output "alb_dns_name" {
   description = "Public DNS name of the Application Load Balancer"

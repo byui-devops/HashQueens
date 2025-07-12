dd5a57e05f725c70047c72e9343dead0f4cb3f4d
Hayley Chavez <104518404+hchavez8@users.noreply.github.com>
2025-07-11 22:14:17 -0600

    Update main.tf
    Removed roles blocks.

--- iac/main.tf
+++ iac/main.tf
@@ -85,10 +85,6 @@ resource "aws_ecs_cluster" "main" {
   name = "task-tracker-cluster"
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
-  role       = aws_iam_role.ecs_task_execution.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
 
 resource "aws_ecs_task_definition" "app" {
   family                   = "task-tracker"
@@ -97,7 +93,6 @@ resource "aws_ecs_task_definition" "app" {
   cpu                      = "256"
   memory                   = "512"
 
-  execution_role_arn = aws_iam_role.ecs_task_execution.arn
 
   container_definitions = jsonencode([
     {

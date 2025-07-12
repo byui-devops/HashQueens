cf6abf8fa4ae694f1a954e63da262872b8687884
Hayley Chavez <104518404+hchavez8@users.noreply.github.com>
2025-07-11 18:20:59 -0600

    Update main.tf
    Added load balancer, target groups, and listener.

--- terraform/main.tf
+++ terraform/main.tf
@@ -116,4 +116,44 @@ resource "aws_ecs_service" "app" {
 
   task_definition = aws_ecs_task_definition.app.arn
 }
+resource "aws_lb" "app_alb" {
+  name               = "task-tracker-alb"
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.public.id]
+  security_groups    = [aws_security_group.ecs_sg.id]
+}
+resource "aws_lb_target_group" "app_tg" {
+  name     = "task-tracker-tg"
+  port     = 8000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  target_type = "ip"
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-399"
+  }
+}
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app_tg.arn
+    container_name   = "task-tracker"
+    container_port   = 8000
+  }
+
+  depends_on = [aws_lb_listener.app_listener]
+
 

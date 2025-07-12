aee3788325663b6f255e13c554bab9d9757949d6
Hayley Chavez <104518404+hchavez8@users.noreply.github.com>
2025-07-11 18:13:13 -0600

    Update main.tf
    Added resource blocks.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,19 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
-
 resource "aws_instance" "app_server" {
   ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2
   instance_type = "t2.micro"
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
 
-  user_data = <<-EOF
+resource "aws_s3_bucket" "my_bucket" {
+  bucket        = "${var.bucket_name}-${random_id.bucket_suffix.hex}"
+  force_destroy = true
+}
+
+user_data = <<-EOF
               #!/bin/bash
               docker run -d -p 80:5000 yourdockerhubusername/group-c-app
             EOF

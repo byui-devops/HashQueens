40046d6cc4c7ccefd60f751623dd53d4f7032472
Hayley Chavez <104518404+hchavez8@users.noreply.github.com>
2025-07-12 09:50:40 -0600

    Update main.tf
    Commented out the IAM role definition blocks, and changed the execution_role_arn to assume the current role.

--- iac/main.tf
+++ iac/main.tf
@@ -104,7 +104,8 @@ resource "aws_ecs_task_definition" "app" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+  execution_role_arn = "arn:aws:iam::196318750163:role/ecsTaskExecutionRole"
+
 
   container_definitions = jsonencode([
     {
@@ -144,25 +145,26 @@ resource "aws_security_group" "ecs_sg" {
 # IAM for ECS Fargate#
 ######################
 
-resource "aws_iam_role" "ecs_task_execution" {
-  name = "ecsTaskExecutionRole"
+#resource "aws_iam_role" "ecs_task_execution" {
+  #name = "ecsTaskExecutionRole"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
+ # assume_role_policy = jsonencode({
+ #   Version = "2012-10-17",
+  #  Statement = [{
+ #     Effect = "Allow",
+ #     Principal = {
+#        Service = "ecs-tasks.amazonaws.com"
+#      },
+#      Action = "sts:AssumeRole"
+ #   }]
+#  })
+#}
 
-resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
-  role       = aws_iam_role.ecs_task_execution.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
+#resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+#  role       = aws_iam_role.ecs_task_execution.name
+#  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+#}
+
 resource "aws_ecs_service" "app" {
   name            = "task-tracker-service"
   cluster         = aws_ecs_cluster.main.id

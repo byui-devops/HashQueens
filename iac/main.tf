55d1d645231bba838b465205afd46f438e1ac5ed
Hayley Chavez <104518404+hchavez8@users.noreply.github.com>
2025-07-15 18:29:05 -0600

    Update main.tf
    Separated CIDR and security group ingress blocks, which are not supposed to be used together, in the load balancer block.

--- iac/main.tf
+++ iac/main.tf
@@ -64,12 +64,22 @@ resource "aws_security_group" "lb_sg" {
   description = "Security group for ALB"
   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # Allow ALB traffic to EC2
+ingress {
+  from_port       = 8000
+  to_port         = 8000
+  protocol        = "tcp"
+  security_groups = [aws_security_group.lb_sg.id]
+}
+
+# (Optional) Allow direct browser access to EC2 for testing
+ingress {
+  from_port   = 8000
+  to_port     = 8000
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
 
   egress {
     from_port   = 0

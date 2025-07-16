52858013e919548e609e71413a88cd93fec45bbc
Hayley Chavez <104518404+hchavez8@users.noreply.github.com>
2025-07-15 19:11:43 -0600

    Update main.tf
    Removed self-referencing line in load balancer security group.

--- iac/main.tf
+++ iac/main.tf
@@ -69,7 +69,6 @@ ingress {
   from_port       = 8000
   to_port         = 8000
   protocol        = "tcp"
-  security_groups = [aws_security_group.lb_sg.id]
 }
 
 # (Optional) Allow direct browser access to EC2 for testing
